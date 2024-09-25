b46ad0464825ff609eba4bb15b0dbb437273cae8
laulajainen <o.laulajainen@proton.me>
2024-09-25 13:56:12 +0200

    Databaseupdater implemented to call on external api to separate meals and ingredients and save them to the database.

---
+++ Foodprep.UpdateDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using Foodprep.UpdateDB;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace FoodPrepApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Updater.UpdateData();
+        }   
+    }
+}
