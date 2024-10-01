67ecc5cd50e71c89dda864c3d69334f1e8ba80fa
EFakLloyd <elisabeth.fak.lloyd@gmail.com>
2024-10-01 14:53:08 +0200

    api call for days

--- Foodprep.API/DayContext.cs
+++ Foodprep.API/DayContext.cs
@@ -14,30 +14,9 @@ namespace Foodprep.API
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-
-            var days = new List<Days>();
-            int weekId = 1;
-            string[] dayNames = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-
-            for (int i = 1; i <= 365; i++)
-            {
-                days.Add(new Days
-                {
-                    daysID = i,
-                    day = dayNames[(i - 1) % 7],
-                    weekID = weekId
-                });
-
-                // Increment weekId every 7 days
-                if (i % 7 == 0)
-                {
-                    weekId++;
-                }
-            }
-            //base.OnModelCreating(modelBuilder);
+            
             modelBuilder.Entity<Days>().HasKey(m => m.daysID);
-            modelBuilder.Entity<Days>().HasData(days);
+            
         }
     }
 }
